9d8e7d930154b91e0ff94de0c1f3f780d263ddc8
tanjoin <tanjoin@users.noreply.github.com>
2023-09-26 17:58:08 +0900

    デザイン変更に伴う修正

--- src/background.js
+++ src/background.js
@@ -1,30 +1,24 @@
-chrome.browserAction.onClicked.addListener((activeTab) => {
-  chrome.tabs.executeScript({
-    file: 'action.js'
-  });
-});
-
-chrome.tabs.onActivated.addListener((tabId, cinfo, tab) => {
-  chrome.tabs.getSelected(null, (tab) => { 
-    switchIconStatus(tab.url);
-  });
-});
-
- 
-chrome.tabs.onUpdated.addListener((tabId, cinfo, tab) => {
-  switchIconStatus(tab.url);
-});
-
 function switchIconStatus(url) {
   if (isStatusPage(url)) {
-    chrome.browserAction.enable();
-    chrome.browserAction.setIcon({path: "img/16.png"});
+    chrome.action.enable();
+    chrome.action.setIcon({path: "img/16.png"});
   } else {
-    chrome.browserAction.disable();
-    chrome.browserAction.setIcon({path: "img/16-disabled.png"});
+    chrome.action.disable();
+    chrome.action.setIcon({path: "img/16-disabled.png"});
   }
 }
 
 function isStatusPage(url) {
   return /.*:\/\/twitter.com\/.*\/status\/.*/.test(url);
 }
+
+chrome.action.onClicked.addListener((activeTab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: activeTab.id },
+    files: ['action.js']
+  });
+});
+ 
+chrome.tabs.onUpdated.addListener((tabId, cinfo, tab) => {
+  switchIconStatus(tab.url);
+});
